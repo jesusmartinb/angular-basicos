30353ff847b8b5e8f2405f8bd5e5dd3043d78697
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2021-08-06 20:25:18 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `Hola, que tal`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
+
+// Tarea:
+// Crear un modulo llamado ContadorModule
+// declaraciones y exportaciones
+
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    // Importar aquí
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
